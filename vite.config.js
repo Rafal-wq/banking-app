fc4ae8e2abdebe7d262f950973139d96941bc1b7
Rafal-wq <rafal_wilczewski@wp.pl>
2025-04-28 17:44:38 +0200

    Fixed Vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/App.jsx',
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
         react(),
     ],
     resolve: {
-    alias: {
-      '@': '/resources/js',
+        alias: {
+            '@': '/resources/js',
+        },
     },
-  },
-  plugins: [
-    laravel({
-      input: ['resources/js/app.jsx'],
-      refresh: true,
-    }),
-  ],
 });
